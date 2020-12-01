3daf2cb38b7029386980e55a86605d752f4bb68d
Evgeniy Zuykin <eugenzuy@gmail.com>
2020-12-01 20:59:30 +0700

    a-click

--- src/main/kotlin/impl/micro/army/ArmyMovementManager.kt
+++ src/main/kotlin/impl/micro/army/ArmyMovementManager.kt
@@ -1,6 +1,7 @@
 package impl.micro.army
 
 import impl.*
+import impl.global.State
 import impl.util.algo.distance
 import impl.util.attackAction
 import impl.util.moveAction
@@ -8,19 +9,27 @@ import model.AutoAttack
 import model.EntityAction
 import model.Vec2Int
 
-private data class ArmyGroup(val ids: Set<Int>)
-
 object ArmyMovementManager : ActionProvider {
     private const val minGroupSize = 10
-    private val groups = mutableListOf<ArmyGroup>()
-    private val unitId2Group = mutableMapOf<Int, ArmyGroup>()
 
     override fun provideActions(): Map<Int, EntityAction> {
-        createGroupIfPossible()?.let { groups.add(it) }
-
         val resultActions = mutableMapOf<Int, EntityAction>()
 
-        //auto attack
+        autoAttack(resultActions)
+
+        myArmy().filter { !resultActions.containsKey(it.id) }.mapNotNull { u ->
+            val e = enemies().minByOrNull { u.distance(it) } ?: return@mapNotNull null
+
+            u.id to if (e.distance(u) < State.maxPathfindNodes)
+                moveAction(e.position, true, false)
+            else
+                attackAction(e, AutoAttack(State.maxPathfindNodes))
+        }.forEach { resultActions[it.first] = it.second }
+
+        return resultActions
+    }
+
+    private fun autoAttack(resultActions: MutableMap<Int, EntityAction>) {
         myArmy().map { u ->
             u to enemies()
                 .map { it to it.distance(u) }
@@ -30,59 +39,5 @@ object ArmyMovementManager : ActionProvider {
         }
             .filter { it.second != null }
             .forEach { (u, e) -> resultActions[u.id] = attackAction(e!!, null) }
-
-        //fresh units movement
-        myArmy()
-            .filter { !resultActions.containsKey(it.id) }
-            .filter { !unitId2Group.containsKey(it.id) }
-            .map { u ->
-                u.id to moveAction(Vec2Int(20, 20))
-            }.forEach { (id, action) -> resultActions[id] = action }
-
-        // move groups to closest enemies
-        groups.toList().forEach { group ->
-            val units = myUnits().filter { group.ids.contains(it.id) }.toList()
-            //if group is empty, remove it
-            if (units.isEmpty()) {
-                groups.remove(group)
-                group.ids.forEach { unitId2Group.remove(it) }
-                return@forEach
-            }
-
-            val groupMiddle = units.asSequence().map { it.position }.reduce { l, r -> l + r } / units.size
-
-//            // if group is to split out
-//            val distanceThreshold = 30
-//            if (units.maxByOrNull { it.position }!!.distance(units.minByOrNull { it.position }!!) < distanceThreshold) {
-//                units.filter { !resultActions.containsKey(it.id) }.forEach {
-//                    resultActions[it.id] =
-//                        moveAction(groupMiddle, true, false)
-//                }
-//                return@forEach
-//            }
-
-            // move to closest enemy
-            val closestEnemy = enemies().minByOrNull { it.distance(groupMiddle) }
-            if (closestEnemy != null) {
-                units.filter { !resultActions.containsKey(it.id) }.forEach {
-                    resultActions[it.id] =
-                        moveAction(closestEnemy.position, true, false)
-                }
-            }
-        }
-
-        return resultActions
-    }
-
-    private fun createGroupIfPossible(): ArmyGroup? {
-        val usedIds = groups.flatMap { it.ids }.toSet()
-        val unbindedArmy = myArmy().map { it.id }.filter { !usedIds.contains(it) }.toSet()
-        if (unbindedArmy.size >= minGroupSize) {
-            val armyGroup = ArmyGroup(unbindedArmy)
-            armyGroup.ids.forEach { unitId2Group[it] = armyGroup }
-            return ArmyGroup(unbindedArmy)
-        }
-        return null
     }
 }
-
