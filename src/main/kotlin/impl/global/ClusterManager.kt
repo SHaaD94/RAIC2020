f93d6a28d96068ec682321205d50f68b55664401
Evgeniy Zuykin <eugenzuy@gmail.com>
2020-12-05 10:13:11 +0700

    fix clustering

--- src/main/kotlin/impl/global/ClusterManager.kt
+++ src/main/kotlin/impl/global/ClusterManager.kt
@@ -22,24 +22,30 @@ object ClusterManager {
 
 
     fun update(playerView: PlayerView) {
-        this.clusters = DBSCANClusterer(
-            playerView.entities.filter { it.damage() > 1 }.toList(), 5, 5.0,
-            object : DistanceMetric<Entity> {
-                override fun calculateDistance(val1: Entity, val2: Entity): Int {
-                    if (val1.playerId != val2.playerId) return 1_000_000
-                    return val1.distance(val2).toInt()
-                }
-            }
-        )
-            .performClustering()
-            .map { unitArray ->
-                Cluster(
-                    unitArray.map { it.id }.toSet(),
-                    unitArray.toList(),
-                    unitArray.fold(Vec2Int(0, 0)) { l, r -> l + r.position } / unitArray.size,
-                    unitArray.first().playerId!!
+        val enitiesToCluster = playerView.entities.filter { it.damage() > 1 }.toList()
+        this.clusters =
+            if (enitiesToCluster.isNotEmpty()) {
+                DBSCANClusterer(
+                    enitiesToCluster, 5, 5.0,
+                    object : DistanceMetric<Entity> {
+                        override fun calculateDistance(val1: Entity, val2: Entity): Int {
+                            if (val1.playerId != val2.playerId) return 1_000_000
+                            return val1.distance(val2).toInt()
+                        }
+                    }
                 )
+                    .performClustering()
+            } else {
+                listOf<List<Entity>>()
             }
+                .map { unitArray ->
+                    Cluster(
+                        unitArray.map { it.id }.toSet(),
+                        unitArray.toList(),
+                        unitArray.fold(Vec2Int(0, 0)) { l, r -> l + r.position } / unitArray.size,
+                        unitArray.first().playerId!!
+                    )
+                }
 
     }
 }
