cfa5e3c35ee4420fc3ae8193a76887bba837f402
Evgeniy Zuykin <eugenzuy@gmail.com>
2020-12-16 21:43:35 +0700

    1

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 mkdir -p release/src
-cat pom.xml | grep -v remove-before-sending > release/pom.xml
+cat pom-prod.xml | grep -v remove-before-sending > release/pom.xml
 cp -r src/main release/src/
 cd release
 zip -r version-$(date +"%m_%d_%Y-%H:%M").zip src/ pom.xml
